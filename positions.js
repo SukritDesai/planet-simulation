99cea4c89f544feeb25db53e672dd90fa4a8290b
Sukrit Desai <sukritdesai@gmail.com>
2022-12-25 19:58:47 -0500

    Added the canvas and a lot of stuff.

--- positions.js
+++ positions.js
@@ -5,8 +5,9 @@
 * IDE: IntelliJ IDEA
 * Description: The necessary functions to determine the positions of a celestial body*/
 
-// Helpers
+const year_length = 365.256898326;
 
+// Helpers
 function deg2rad(deg) {
     return deg * (Math.PI / 180);
 }
@@ -15,50 +16,50 @@ function rad2deg(rad) {
     return rad * (Math.PI / 180);
 }
 
-class Body {
-    year_length = 365.256898326;
+function rev(x) {
+    return x - Math.floor(x / 360) * 360;
+}
 
-    perihelion_distance(a, e) {
-        return a * (1 - e);
-    }
+function perihelion_distance(a, e) {
+    return a * (1 - e);
+}
 
-    aphelion_distance(a, e) {
-        return a * (1 + e);
-    }
+function aphelion_distance(a, e) {
+    return a * (1 + e);
+}
 
-    orbital_period(a, m) {
-        return this.year_length * a ** 1.5 / Math.sqrt(1 + m);
-    }
+function orbital_period(a, m) {
+    return this.year_length * a ** 1.5 / Math.sqrt(1 + m);
+}
 
-    daily_motion(P) {
-        return 360 / P;
-    }
+function daily_motion(P) {
+    return 360 / P;
+}
 
-    days() {
+function days(y, m, D) {
+    return 367 * y - 7 * (y + (m+9) /12 ) / 4 - 3 * ((y + (m-9)/7 ) / 100 + 1) / 4 + 275 * m/9 + D - 730515
+}
 
-    }
+function ptime() {
 
-    ptime() {
+}
 
-    }
+function mean_anomaly(t, T, P) {
+    return (t - T) * 360 / P;
+}
 
-    mean_anomaly(t, T, P) {
-        return (t - T) * 360 / P;
-    }
+function mean_longitude(M, w, N) {
+    return M + w + N;
+}
 
-    mean_longitude(M, w, N) {
-        return M + w + N;
-    }
+function ecc_anomaly() {
 
-    ecc_anomaly() {
+}
 
-    }
+function true_anomaly() {
 
-    true_anomaly() {
+}
 
-    }
+function hel_distance() {
 
-    hel_distance() {
-
-    }
 }
